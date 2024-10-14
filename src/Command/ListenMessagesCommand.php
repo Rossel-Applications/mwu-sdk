cb63ac8ddca1a356b9a6dced011cc672ad54eaf3
VANESTE <florian.vaneste@rossel.fr>
2024-10-14 15:54:13 +0200

    wip

--- src/Command/ListenMessagesCommand.php
+++ src/Command/ListenMessagesCommand.php
@@ -23,18 +23,16 @@ final class ListenMessagesCommand extends Command
     {
         $output->writeln('Start listening...');
 
-        dump($this->mwuService);
         $switches = $this->mwuService->getSwitches();
-        dump($switches);
-
         $sockets = [];
 
         foreach ($switches as $switch) {
+            $output->writeln("Start listening to $switchIp...");
+
             $socket = socket_create(\AF_INET, \SOCK_STREAM, \SOL_TCP);
             $sockets[] = $socket;
             socket_bind($socket, $switchIp = $switch->getIpAddress(), $switch->getPort());
             socket_listen($socket);
-            $output->writeln("Start listening to $switchIp...");
         }
 
         set_time_limit(0);
