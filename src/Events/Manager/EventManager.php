1e9ea589cba98311515a84b490445e6bbe4692eb
VANESTE <florian.vaneste@rossel.fr>
2024-10-16 16:44:26 +0200

    events

--- src/Events/Manager/EventManager.php
+++ src/Events/Manager/EventManager.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace MwuSdk\Events\Manager;
 
+use MwuSdk\Client\TcpIp\TcpIpClientInterface;
+use MwuSdk\Entity\Command\ClientCommand\Ack\AckCommand;
+use MwuSdk\Entity\Message\ClientMessage\ClientMessage;
 use MwuSdk\Events\Event\EventInterface;
+use MwuSdk\Events\Event\MessageReceivedEventInterface;
 use MwuSdk\Events\Listener\EventListenerInterface;
+use MwuSdk\Exception\Client\TcpIp\TcpIpClientExceptionInterface;
+use Random\RandomException;
 
 final class EventManager implements EventManagerInterface
 {
@@ -13,7 +19,8 @@ final class EventManager implements EventManagerInterface
      * @param array<int, EventListenerInterface> $eventListeners
      */
     public function __construct(
-        private array $eventListeners = [],
+        private array $eventListeners,
+        private readonly TcpIpClientInterface $tcpIpClient,
     ) {
     }
 
@@ -34,12 +41,33 @@ final class EventManager implements EventManagerInterface
         $this->eventListeners = array_values($this->eventListeners);
     }
 
+    /**
+     * @throws RandomException
+     * @throws TcpIpClientExceptionInterface
+     */
     public function handleEvent(EventInterface $eventData): void
     {
+        if ($eventData instanceof MessageReceivedEventInterface) {
+            $this->ackOnMessageReception($eventData);
+        }
+
         foreach ($this->eventListeners as $listener) {
             if ($listener->supports($eventData)) {
                 $listener->handleEvent($eventData);
             }
         }
+    }
+
+    /**
+     * @throws RandomException
+     * @throws TcpIpClientExceptionInterface
+     */
+    private function ackOnMessageReception(MessageReceivedEventInterface $messageReceivedEvent): void
+    {
+        $sequenceNumber = $messageReceivedEvent->getMessage()->getSequenceNumber();
+
+        $ackMessage = new ClientMessage(new AckCommand(), $sequenceNumber);
+
+        $this->tcpIpClient->sendMessage((string) $ackMessage);
     }
 }
