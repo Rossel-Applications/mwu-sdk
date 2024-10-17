1eea09fd0316054b4669cd34b2e0594dd48135e7
VANESTE <florian.vaneste@rossel.fr>
2024-10-17 14:37:41 +0200

    events

--- src/DependencyInjection/Compiler/AddEventListenerTagPass.php
+++ src/DependencyInjection/Compiler/AddEventListenerTagPass.php
@@ -7,28 +7,17 @@ namespace MwuSdk\DependencyInjection\Compiler;
 use MwuSdk\Events\Listener\EventListenerInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 
 class AddEventListenerTagPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
         foreach ($container->getDefinitions() as $definition) {
-            dump($definition);
-            if ($this->implementsEventListenerInterface($definition)) {
+            if ($definition->getClass()
+                && is_subclass_of($definition->getClass(), EventListenerInterface::class)
+            ) {
                 $definition->addTag('mwu_sdk.event_listener');
             }
         }
-    }
-
-    private function implementsEventListenerInterface(Definition $definition): bool
-    {
-        $class = $definition->getClass();
-
-        if (!$class) {
-            return false;
-        }
-
-        return is_subclass_of($class, EventListenerInterface::class);
     }
 }
